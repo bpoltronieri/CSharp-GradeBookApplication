497798fef825f09507b1158693bff18b3c6e23b7
Bruno Poltronieri <brunopoltronieri@hotmail.com>
2020-10-30 01:28:08 +0000

    Remove unnecessary using directives.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Linq;
-
 using GradeBook.Enums;
-using System.Collections.Generic;
-using System.IO;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
